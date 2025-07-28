eda444212ea9f63890e9ead4a3421a5c86394701
ParkHun <hundl5789@gmail.com>
2025-07-28 12:41:30 +0900

    [feature] LeaderBoard Model 추가

---
+++ G9/Assets/Scripts/LeaderBoard/LeaderBoardData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G9.Game.LeaderBoard
+{
+    [Serializable]
+    public class LeaderBoardData
+    {
+        public string gameId;
+        public string displayName;
+        public List<LeaderBoardEntry> entries = new();
+    }
+}
---
+++ G9/Assets/Scripts/LeaderBoard/LeaderBoardEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G9.Game.LeaderBoard
+{
+    [Serializable]
+    public class LeaderBoardEntry
+    {
+        public string playerName;
+        public int score;
+        public float playTime;
+        public string timestamp; // 기록 시간 (옵션)
+
+        // 게임별 특수 데이터 (ex. "maxCombo": "25", "perfectClear": "true")
+        public Dictionary<string, string> extraData = new();
+    }
+}
